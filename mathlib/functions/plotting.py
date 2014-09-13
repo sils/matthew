a6c1e10b3bb83650ea4349f0dfa9e79507c99da2
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-09-13 19:13:09 +0200

    plotting: Output modifications

--- mathlib/functions/plotting.py
+++ mathlib/functions/plotting.py
@@ -9,9 +9,11 @@ def plot(*args, glob_vars={}):
     ylabel = ''
     for arg in args:
         ylabel += " "+arg
+    ylabel = ylabel.strip()
     pyplot.ylabel(ylabel)
 
     if len(unknowns) == 0:
+        print("Plotting '{}' with no variables...".format(ylabel))
         x = [-10,10]
         y = eval(func_str)
         pyplot.plot(x, [y, y])
@@ -20,23 +22,25 @@ def plot(*args, glob_vars={}):
         return True
 
     if len(unknowns) == 1:
-        glob_vars[unknowns[0]] = 0
+        unknown = unknowns[0]
+        print("Plotting '{}' with variable '{}'...".format(ylabel, unknown))
+        glob_vars[unknown] = 0
         y = []
         x = []
         for x_scaled in range(-100, 100, 1):
-            glob_vars[unknowns[0]] = x_scaled/10
-            x.append(glob_vars[unknowns[0]])
+            glob_vars[unknown] = x_scaled/10
+            x.append(glob_vars[unknown])
             y.append(eval(func_str))
 
         pyplot.plot(x, y)
-        pyplot.xlabel(unknowns[0])
+        pyplot.xlabel(unknown)
         pyplot.grid(True)
 
         pyplot.show()
 
-        del glob_vars[unknowns[0]]
+        del glob_vars[unknown]
         return True
 
     print("Too many unknown variables. Only up to one dimensional functions can be plotted.")
-    print(unknowns)
+    print("Unknown variables are:", unknowns)
     return False
