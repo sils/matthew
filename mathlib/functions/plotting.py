0088f89f0829d2ed1f55b86a3c72846007aa84e4
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-09-13 20:01:22 +0200

    plotting: Handle evaluation failures

--- mathlib/functions/plotting.py
+++ mathlib/functions/plotting.py
@@ -15,11 +15,14 @@ def plot(*args, glob_vars={}):
     if len(unknowns) == 0:
         print("Plotting '{}' with no variables...".format(ylabel))
         x = [-10,10]
-        y = eval(func_str)
-        pyplot.plot(x, [y, y])
-        pyplot.grid(True)
-        pyplot.show()
-        return True
+        try:
+            y = eval(func_str)
+            pyplot.plot(x, [y, y])
+            pyplot.grid(True)
+            pyplot.show()
+            return True
+        except:
+            print("Cannot evaluate expresssion. Aborting plot...")
 
     if len(unknowns) == 1:
         unknown = unknowns[0]
@@ -27,10 +30,22 @@ def plot(*args, glob_vars={}):
         glob_vars[unknown] = 0
         y = []
         x = []
+        evaluation_failures = 0
         for x_scaled in range(-100, 100, 1):
             glob_vars[unknown] = x_scaled/10
-            x.append(glob_vars[unknown])
-            y.append(eval(func_str))
+            try:
+                s = eval(func_str)
+                x.append(glob_vars[unknown])
+                y.append(s)
+                evaluation_failures = 0
+            except:
+                print("Failed evaluating function for {}={}".format(unknown, glob_vars[unknown]))
+                evaluation_failures += 1
+                if evaluation_failures > 10:
+                    print("There were 10 evaluation failures in a row. "
+                          "Assuming function is not plottable. Aborting plot...")
+                    del glob_vars[unknown]
+                    return False
 
         pyplot.plot(x, y)
         pyplot.xlabel(unknown)
