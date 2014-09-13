dd4282bde17a8ffd47a3cde17b87e3f8cf22ea88
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-09-13 18:13:17 +0200

    general: Detect unknown variables

--- mathlib/functions/general.py
+++ mathlib/functions/general.py
@@ -24,40 +24,63 @@ def _print(*args, glob_vars={}):
 
 
 def _return(*args, glob_vars={}):
-    return save_eval(*args, glob_vars=glob_vars)
+    s, ok = generate_function(*args, glob_vars=glob_vars)
+    return eval(s)
 
 
-def get_value(var, glob_vars={}):
-    var = str(var)
-    if not var in glob_vars:
-        return var
+def makevar(variable, glob_vars={}):
+    if variable.strip() == "":
+        return " ", True
+    if variable in glob_vars:
+        return "glob_vars[\""+variable+"\"]", True
 
-    return glob_vars[var]
+    if variable.isnumeric():
+        return variable, True
+
+    return variable, False
 
 
 variable_chars = string.ascii_letters + "_" + string.digits
-operator_chars = "+-*/%"
-allowed_chars = variable_chars + operator_chars
-def save_eval(*args, glob_vars={}):
+operator_chars = "+-*/%()"
+def generate_function(*args, glob_vars={}):
     save_str = ""
+    unknown_vars = []
     for arg in args:
         variable = ""
         for char in arg:
-            if char in allowed_chars:
-                if char in operator_chars:
-                    save_str += str(get_value(variable, glob_vars)) + char
-                    variable = ""
-                else:
-                    variable += char
+            if char in operator_chars:
+                s, allok = makevar(variable, glob_vars)
+                if not allok:
+                    unknown_vars.append(s)
+                save_str += s + char
+                variable = ""
+            else:
+                variable += char
 
-        save_str += str(get_value(variable, glob_vars))
+        s, allok = makevar(variable, glob_vars)
+        if not allok:
+            unknown_vars.append(s)
+        save_str += s
+    return save_str, unknown_vars
 
-    return eval(save_str)
+
+def save_eval(*args, glob_vars={}):
+    val, unknown = generate_function(*args, glob_vars=glob_vars)
+    if len(unknown) > 0:
+        print("The following variables are undefined:")
+        for var in unknown:
+            print("  {}".format(var))
+        print("Evaluation not possible.")
+        return None
+
+    return eval(val)
 
 
 def let(var, be, *vals, glob_vars={}):
     if len(vals) < 1 or be.lower() != "be":
         print("Invalid syntax: use 'let <yourvar> be <yourval(s)>'")
 
-    glob_vars[var] = save_eval(*vals, glob_vars=glob_vars)
-    return glob_vars[var]
+    val = save_eval(*vals, glob_vars=glob_vars)
+
+    glob_vars[var] = val
+    return val
