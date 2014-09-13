74bfa374955ef6c0595ea9b6abc1119dbae70d13
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-09-13 13:41:41 +0200

    Add LineExecutor
    This adds a very simple shell which can print and exit.

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from mathlib.parsing.LineExecutor import LineExecutor
+
 __author__ = 'lasse'
 
+
+def _print(*args, glob_vars={}):
+    print(*args)
+
+def _exit(glob_vars={}):
+    exit(0)
+
 if __name__ == '__main__':
-    pass
+    le = LineExecutor(commands={
+        "print": _print,
+        "exit": _exit
+    })
+
+    prompt = "> "
+    while True:
+        le.exec_line(input(prompt))
---
+++ mathlib/parsing/LineExecutor.py
@@ -0,0 +1,32 @@
+__author__ = 'lasse'
+
+
+class LineExecutor:
+    ANS = "ans"
+
+    def __init__(self, commands={}, glob_vars={}):
+        """
+        :param commands: dictionary with command (string) as index and function pointer as value
+        :param vars: dictionary with variable name (string) as index and variable as value
+        """
+        self.commands = commands
+        self.glob_vars = glob_vars
+
+    def exec_line(self, line):
+        components = str(line).strip().lower().split(" ")
+        if len(components) < 1:
+            return None
+
+        command = components[0]
+        args = components[1:]
+
+        if not command in self.commands:
+            print("This command '{}' is unsupported.".format(command))
+            return None
+
+        retval = self.commands[command](*args, glob_vars=self.glob_vars)
+        self.glob_vars[self.ANS] = retval
+
+        print("Command '{}' returned: {} = {}".format(command, self.ANS, retval))
+
+        return retval
