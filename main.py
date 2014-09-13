c6f4a68806aafb19f059e96d2e5f2c12dded0913
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-09-13 19:15:27 +0200

    main: Add more useful functions and constants

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import sin, exp
+import math
 from mathlib.functions.all import commands
 from mathlib.parsing.LineExecutor import LineExecutor
 
@@ -8,8 +8,17 @@ if __name__ == '__main__':
     le = LineExecutor(commands=commands,
                       glob_vars={
                           "ans": None,
-                          "sin": sin,
-                          "exp": exp,
+                          "sin": math.sin,
+                          "cos": math.cos,
+                          "sinh": math.sinh,
+                          "cosh": math.cosh,
+                          "asin": math.asin,
+                          "acos": math.acos,
+                          "asinh": math.asinh,
+                          "acosh": math.acosh,
+                          "exp": math.exp,
+                          "e": math.e,
+                          "pi": math.pi,
                           "pow": pow
                       })
 
